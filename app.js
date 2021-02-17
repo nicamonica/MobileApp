b1b87ea1147ebf14865e98af69d75f7f1e6f3d0c
nicamonica <60478231+nicamonica@users.noreply.github.com>
2021-02-17 17:33:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//html buttons for JS
+
 const avocadoButton = document.querySelector('#btn-avocado');
 const beansButton = document.querySelector('#btn-beans');
 const cheeseButton = document.querySelector('#btn-cheese');
@@ -9,8 +11,11 @@ const tomatoButton = document.querySelector('#btn-tomato');
 
 const displayLabel = document.querySelector('#lbl-output');
 
+// function
 let product = "";
 let nutrition = "";
+
+// js buttons for next step
 
 avocadoButton.addEventListener('click', assignAvocado);
 beansButton.addEventListener('click', assignBeans);
@@ -21,56 +26,60 @@ fishButton.addEventListener('click', assignFish);
 potatoesButton.addEventListener('click', assignPotatoes);
 tomatoButton.addEventListener('click', assignTomato);
 
+// functions for buttons
+
 function assignAvocado(){
-    product = "Avocado";
-    nutrition = "tbd";
+    product = "Avocado (100g)";
+    nutrition = "160 calories";
     updateLabel()
 } 
 
 function assignBeans(){
-    product = "Beans";
-    nutrition = "tbd";
+    product = "Beans(100g)";
+    nutrition = "347 calories";
     updateLabel()
 } 
 
 function assignCheese(){
-    animal = "Cheese";
-    sound = "tbd";
+    product = "Cheese(100g)";
+    nutrition = "402 calories";
     updateLabel()
 } 
 
 function assignChicken(){
-    animal = "Chicken";
-    sound = "tbd";
+    product = "Chicken(100g)";
+    nutrition = "239 calories";
     updateLabel()
 } 
 
 function assignEggs(){
-    animal = "Eggs";
-    sound = "tbd";
+    product = "Eggs(100g)";
+    nutrition = "155 calories";
     updateLabel()
 } 
 
 function assignFish(){
-    animal = "Fish";
-    sound = "tbd";
+    product = "Fish(100g)";
+    nutrition = "206 calories";
     updateLabel()
 } 
 
 function assignPotatoes(){
-    animal = "Potatoes";
-    sound = "tbd";
+    product = "Potatoes(100g)";
+    nutrition = "313 calories";
     updateLabel()
 } 
 
 function assignTomato(){
-    animal = "Tomato";
-    sound = "tbd";
+    product = "Tomato(100g)";
+    nutrition = "18 calories";
     updateLabel()
 } 
 
+// output with a sentence
+
 function updateLabel(){
-    let output = `That ${product} has ${nutrition}!`;
+    let output = `That ${product} has ${nutrition}.`;
     console.log(output);
     displayLabel.textContent = output;
 
